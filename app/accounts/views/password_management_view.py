010092a8692422bfb35088850f2269669e77c748
selim-reza-UITS <selim.reza.uits@gmail.com>
2025-09-28 04:24:10 +0600

    email host update

--- app/accounts/views/password_management_view.py
+++ app/accounts/views/password_management_view.py
@@ -10,26 +10,33 @@ from rest_framework.views import APIView
 
 from app.accounts.models import PasswordResetOTP
 from app.accounts.serializers.password_serializers import (
-    ChangePasswordSerializer, RequestOTPSerializer, ResetPasswordSerializer,
-    VerifyOTPSerializer)
+    ChangePasswordSerializer,
+    RequestOTPSerializer,
+    ResetPasswordSerializer,
+    VerifyOTPSerializer,
+)
+from _core.settings.local import EMAIL_HOST_USER
 
 User = get_user_model()
+
 
 class RequestOTPView(APIView):
     @swagger_auto_schema(
         request_body=RequestOTPSerializer,
         operation_summary="Request For OTP",
-        operation_description="Request to send OTP to email"
+        operation_description="Request to send OTP to email",
     )
     def post(self, request):
         serializer = RequestOTPSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        email = serializer.validated_data['email']
+        email = serializer.validated_data["email"]
         try:
             user = User.objects.get(email=email)
         except User.DoesNotExist:
-            return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"error": "User not found."}, status=status.HTTP_404_NOT_FOUND
+            )
 
         otp = str(random.randint(1234, 9999))
         PasswordResetOTP.objects.create(user=user, otp=otp)
@@ -37,16 +44,16 @@ class RequestOTPView(APIView):
         send_mail(
             subject="Password Reset OTP",
             message=f"Your OTP is: {otp}",
-            from_email="noreply@ingridienta.com",
-            recipient_list=[email]
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[email],
         )
 
-        return Response({"message": "OTP sent to your email."}, status=status.HTTP_200_OK)
+        return Response(
+            {"message": "OTP sent to your email."}, status=status.HTTP_200_OK
+        )
 
 
 class VerifyOTPView(APIView):
-    
-    
     @swagger_auto_schema(
         operation_summary="OTP Verification",
         operation_description="Verify requested OTP",
@@ -56,12 +63,14 @@ class VerifyOTPView(APIView):
         serializer = VerifyOTPSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        email = serializer.validated_data['email']
-        otp = serializer.validated_data['otp']
+        email = serializer.validated_data["email"]
+        otp = serializer.validated_data["otp"]
 
         try:
             user = User.objects.get(email=email)
-            otp_obj = PasswordResetOTP.objects.filter(user=user, otp=otp, is_used=False).latest("created_at")
+            otp_obj = PasswordResetOTP.objects.filter(
+                user=user, otp=otp, is_used=False
+            ).latest("created_at")
         except (User.DoesNotExist, PasswordResetOTP.DoesNotExist):
             return Response({"error": "Invalid OTP or email."}, status=400)
 
@@ -81,13 +90,15 @@ class ResetPasswordView(APIView):
         serializer = ResetPasswordSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        email = serializer.validated_data['email']
-        otp = serializer.validated_data['otp']
-        new_password = serializer.validated_data['new_password']
+        email = serializer.validated_data["email"]
+        otp = serializer.validated_data["otp"]
+        new_password = serializer.validated_data["new_password"]
 
         try:
             user = User.objects.get(email=email)
-            otp_obj = PasswordResetOTP.objects.filter(user=user, otp=otp, is_used=False).latest("created_at")
+            otp_obj = PasswordResetOTP.objects.filter(
+                user=user, otp=otp, is_used=False
+            ).latest("created_at")
         except (User.DoesNotExist, PasswordResetOTP.DoesNotExist):
             return Response({"error": "Invalid OTP or email."}, status=400)
 
@@ -102,11 +113,10 @@ class ResetPasswordView(APIView):
         return Response({"message": "Password reset successfully."}, status=200)
 
 
-
 class ChangePasswordView(generics.UpdateAPIView):
     serializer_class = ChangePasswordSerializer
     permission_classes = [permissions.IsAuthenticated]
-    http_method_names = ['put'] 
+    http_method_names = ["put"]
 
     def get_object(self):
         return self.request.user
@@ -115,10 +125,14 @@ class ChangePasswordView(generics.UpdateAPIView):
         operation_summary="Update Password",
         operation_description="Update the user's password by validating the current one.",
         request_body=ChangePasswordSerializer,
-        responses={200: "Password changed successfully", 400: "Validation error"}
+        responses={200: "Password changed successfully", 400: "Validation error"},
     )
     def update(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data, context={'request': request})
+        serializer = self.get_serializer(
+            data=request.data, context={"request": request}
+        )
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response({"detail": "Password changed successfully"}, status=status.HTTP_200_OK)
+        return Response(
+            {"detail": "Password changed successfully"}, status=status.HTTP_200_OK
+        )
