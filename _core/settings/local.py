31be4c1c863dc12b665b24afddba0349566010eb
selim-reza-UITS <selim.reza.uits@gmail.com>
2025-09-28 03:57:11 +0600

    update

--- _core/settings/local.py
+++ _core/settings/local.py
@@ -69,11 +69,12 @@ STATICFILES_DIRS = [
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "srreza1999@gmail.com"
-EMAIL_HOST_PASSWORD = "thvljalqrgiucbgx"
+EMAIL_HOST = "smtp.hostinger.com"
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CACHES = LOCAL_CACHE_CONFIG
